c3a2b258023b1df97ffaf321a3d5ebc63d841041
Rubens Tanioka <87711124+rutanioka@users.noreply.github.com>
2022-12-10 16:34:40 -0300

    upload de arquivos presetados pelo FC

--- Consolidador/internal/infra/db/db.go
+++ Consolidador/internal/infra/db/db.go
@@ -5,4 +5,22 @@ import(
     "database/sql"
 )
 
-type DBTX interface
+type DBTX interface {
+    ExecContext(context.Context, string, ...interface{}) (sql.Result, error)
+    PrepareContext(context.Context, string) (*sql.Stmt, error)
+    QueryContext(context.Context, string, ...interface{}) ()
+}
+
+func New(db DBTX) *Queries {
+	return &Queries{db: db}
+}
+
+type Queries struct {
+	db DBTX
+}
+
+func (q *Queries) WithTx(tx *sql.Tx) *Queries {
+	return &Queries{
+		db: tx,
+	}
+}
