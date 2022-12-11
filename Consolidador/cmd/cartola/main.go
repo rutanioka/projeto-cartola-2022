7c0d3d611493655da914da1a3ffff1dc8b9e1dac
Rubens Tanioka <87711124+rutanioka@users.noreply.github.com>
2022-12-11 21:57:51 +0000

    update 19:00

---
+++ Consolidador/cmd/cartola/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/rutanioka/Projeto-Cartola-2022/Consolidador/internal/domain/entity/repository"
+	"github.com/rutanioka/Projeto-Cartola-2022/Consolidador/package/uow"
+	"github.com/rutanioka/Projeto-Cartola-2022/Consolidador/internal/infra/db"
+)
+
+func main()  {
+	ctx := context.Background()
+	dtb, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/cartola?parseTime=true")
+	if err != nil{
+		panic(err)
+	}
+	defer dtb.Close()
+	uow, err := uow.NewUow(ctx, dtb)
+	if err !=nil{
+		panic(err)
+	}
+	registerRepositories(uow)
+
+}
+
+func registerRepositories(uow *uow.Uow)  {
+	uow.Register("PlayerRepository", func (tx *sql.Tx) interface{} {
+		repo := repository.NewPlayerRepository(uow.Db)
+		repo.Queries = db.New(tx)
+		return repo
+	})
+	
+	uow.Register("MatchRepository", func(tx *sql.Tx) interface{} {
+		repo := repository.NewMatchRepository(uow.Db)
+		repo.Queries = db.New(tx)
+		return repo
+	})
+
+	uow.Register("TeamRepository", func(tx *sql.Tx) interface{} {
+		repo := repository.NewTeamRepository(uow.Db)
+		repo.Queries = db.New(tx)
+		return repo
+	})
+
+	uow.Register("MyTeamRepository", func(tx *sql.Tx) interface{} {
+		repo := repository.NewMyTeamRepository(uow.Db)
+		repo.Queries = db.New(tx)
+		return repo
+	})
+}
---
+++ Consolidador/internal/usecase/match_update_result.go
@@ -0,0 +1,59 @@
+package usecase
+
+import (
+	"context"
+	"strconv"
+	"strings"
+
+	"github.com/rutanioka/Projeto-Cartola-2022/Consolidador/internal/domain/entity"
+	"github.com/rutanioka/Projeto-Cartola-2022/Consolidador/internal/domain/entity/repository"
+	"github.com/rutanioka/Projeto-Cartola-2022/Consolidador/package/uow"
+)
+
+
+
+
+
+type MatchUpdateResultInput struct {
+	ID     string `json:"match_id"`
+	Result string `json:"result"`
+}
+
+type MatchUpdateResultUseCase struct {
+	Uow uow.UowInterface
+}
+
+func NewMatchUpdateResultUseCase(uow uow.UowInterface) *MatchUpdateResultUseCase {
+	return &MatchUpdateResultUseCase{
+		Uow: uow,
+	}
+}
+
+func (u *MatchUpdateResultUseCase) Execute(ctx context.Context, input MatchUpdateResultInput) error {
+	err := u.Uow.Do(ctx, func(_ *uow.Uow) error {
+		matchRepo := u.getMatchRepository(ctx)
+		match, err := matchRepo.FindByID(ctx, input.ID)
+		if err != nil {
+			return err
+		}
+		matchResult := strings.Split(input.Result, "-")
+		// convert results to int
+		teamAResult, _ := strconv.Atoi(matchResult[0])
+		teamBResult, _ := strconv.Atoi(matchResult[1])
+		match.Result = *entity.NewMatchResult(teamAResult, teamBResult)
+		err = matchRepo.Update(ctx, match)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	return err
+}
+
+func (u *MatchUpdateResultUseCase) getMatchRepository(ctx context.Context) repository.MatchRepositoryInterface {
+	matchRepository, err := u.Uow.GetRepository(ctx, "MatchRepository")
+	if err != nil {
+		panic(err)
+	}
+	return matchRepository.(repository.MatchRepositoryInterface)
+}
