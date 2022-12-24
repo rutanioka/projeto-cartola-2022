1fc949b3f302bc89b8d7791f3efd74ec93d58ff6
Rubens Tanioka <87711124+rutanioka@users.noreply.github.com>
2022-12-24 01:34:23 +0000

    update choose_team use case

--- Consolidador/internal/infra/kafka/event/choose_team.go
+++ Consolidador/internal/infra/kafka/event/choose_team.go
@@ -1,10 +1,26 @@
 package event
 
-import "context"
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/rutanioka/Projeto-Cartola-2022/Consolidador/internal/usecase"
+	"github.com/rutanioka/Projeto-Cartola-2022/Consolidador/package/uow"
+)
 
 
 type ProcessChooseTeam struct{}
 
-func (p ProcessChooseTeam) Process (ctx context.Context]) {
-	
+func (p ProcessChooseTeam) Process (ctx context.Context, msg *kafka.Message, uow uow.UowInterface)error {
+	var input usecase.MyTeamChoosePlayersInput
+	err := json.Unmarshal(msg.Value, &input)
+	if err != nil{
+		return err
+	}
+	addNewTeamUseCase := usecase.NewMyTeamChoosePlayersUseCase(uow)
+	err = addNewTeamUseCase.Execute(ctx, input)
+	if err != nil {
+		return err
+	}
+	return nil
 }
