a9da25b0ee23ed93bb318195e1b4f0a3fedfb080
Rubens Tanioka <87711124+rutanioka@users.noreply.github.com>
2022-12-24 02:03:48 +0000

    update match update result

---
+++ Consolidador/internal/infra/kafka/event/match_update_result.go
@@ -0,0 +1,28 @@
+package event
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/rutanioka/Projeto-Cartola-2022/Consolidador/internal/usecase"
+	"github.com/rutanioka/Projeto-Cartola-2022/Consolidador/package/uow"
+)
+
+
+type ProcessMatchUpdateResult struct {}
+
+func (p ProcessMatchUpdateResult) Process (ctx context.Context, msg *kafka.Message, uow uow.UowInterface) error {
+	var input usecase.MatchUpdateResultInput
+	err := json.Unmarshal(msg.Value, &input)
+	if err != nil{
+		return err
+	}
+	fmt.Println("input", input)
+	updateMatchResultUseCase := usecase.NewMatchUpdateResultUseCase(uow)
+	err = updateMatchResultUseCase.Execute(ctx, input)
+	if err != nil {
+		return err
+	}
+	return nil
+}
