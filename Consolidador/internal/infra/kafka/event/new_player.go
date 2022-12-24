ee086fef78f98f2c0bab4561d2a6eb606fcdd459
Rubens Tanioka <87711124+rutanioka@users.noreply.github.com>
2022-12-24 05:12:47 +0000

    ...

--- Consolidador/internal/infra/kafka/event/new_player.go
+++ Consolidador/internal/infra/kafka/event/new_player.go
@@ -13,7 +13,7 @@ import (
 type ProcessNewPlayer struct{}
 
 func (p ProcessNewPlayer) Process(ctx context.Context, msg *kafka.Message, uow uow.UowInterface)error {
-	var input usecase.AddNewPlayerInput
+	var input usecase.AddPlayerInput
 	err := json.Unmarshal(msg.Value, &input)
 	if err != nil{
 		return err
