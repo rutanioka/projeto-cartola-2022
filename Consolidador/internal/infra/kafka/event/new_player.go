6705b46979c4aa36c40959c1b949965312113494
Rubens Tanioka <87711124+rutanioka@users.noreply.github.com>
2022-12-24 02:29:37 +0000

    write new_player.go

---
+++ Consolidador/internal/infra/kafka/event/new_player.go
@@ -0,0 +1,22 @@
+package event
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/rutanioka/Projeto-Cartola-2022/Consolidador/internal/usecase"
+	"github.com/rutanioka/Projeto-Cartola-2022/Consolidador/package/uow"
+)
+
+
+type ProcessNewPlayer struct{}
+
+func (p ProcessNewPlayer) Process(ctx context.Context, msg *kafka.Message, uow uow.UowInterface)error {
+	var input usecase.AddNewPlayerInput
+	err := json.Unmarshal(msg.Value, &input)
+	if err != nil{
+		return err
+	}
+	return nil
+}
